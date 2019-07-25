7140c6cba7fabc1ec3554c8f25b509f93c8c029c
leopheard <leopheard.misc@gmail.com>
2019-07-24 22:25:12 -0400

    uploads

---
+++ README.md
@@ -0,0 +1,13 @@
+#Chapo Trap House podcasts<br>
+=============================
+
+<a href="www.kodi.tv">KODI</a> / XBMC / Firestick audio addon for many of the <a href="https://www.npr.org/podcasts/">Chapo Trap House podcasts</a>.<br>
+
+Kodi (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
+
+<a href="https://chapotraphouse.com/">ChapoTrapHouse.com</a>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+
+<a href="https://patreon.com/chapotraphouse"><img src="https://i1.sndcdn.com/avatars-000230770726-ib4tc4-t500x500.jpg" alt="Chapo Trap House"><br>
+<img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
---
+++ addon.py
@@ -0,0 +1,58 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import chapotraphouse
+import requests
+import re
+from bs4 import BeautifulSoup
+
+plugin = Plugin()
+
+URL = "https://feeds.soundcloud.com/users/soundcloud:users:211911700/sounds.rss"
+
+@plugin.route('/')
+def main_menu():
+    """
+    main menu 
+    """
+    items = [
+        {
+            'label': plugin.get_string(30000), 
+            'path': plugin.url_for('all_episodes'),
+            'thumbnail': "https://i1.sndcdn.com/avatars-000230770726-ib4tc4-t500x500.jpg"},
+   {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "https://i1.sndcdn.com/avatars-000230770726-ib4tc4-t500x500.jpg"},
+    ]
+
+    return items
+
+@plugin.route('/all_episodes/')
+def all_episodes():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = chapotraphouse.get_soup(URL)
+    
+    playable_podcast = chapotraphouse.get_playable_podcast(soup)
+    
+    items = chapotraphouse.compile_playable_podcast(playable_podcast)
+
+    return items
+
+
+@plugin.route('/all_episodes1/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = chapotraphouse.get_soup(URL)
+    
+    playable_podcast1 = chapotraphouse.get_playable_podcast1(soup)
+    
+    items = chapotraphouse.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+
+if __name__ == '__main__':
+    plugin.run()
